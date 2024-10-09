b802474280ac38e04d9ae604bad8604a921b1161
capmahin <mahinulhuque1996@gmail.com>
2024-10-10 03:02:46 +0600

    call init function

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ const kafka = new Kafka({
   brokers: ["http://192.168.0.102:9092"]
 });
 
-async function Infinity() {
+async function init() {
   const admin = kafka.admin();
   console.log("Admin connecting...");
   admin.connect();
@@ -22,5 +22,8 @@ async function Infinity() {
   });
   console.log(" Topic Created Success [rider-updates]");
 
+  console.log("Disconnecting Admin..");
   await admin.disconnect();
 }
+
+init();
