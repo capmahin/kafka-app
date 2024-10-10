35d085564ccde6fd410512f291815fc21dc9d018
capmahin <mahinulhuque1996@gmail.com>
2024-10-11 01:33:07 +0600

    riderupdates

--- consumer.js
+++ consumer.js
@@ -3,4 +3,6 @@ const { kafka } = require("./client");
 async function init() {
   const consumer = kafka.consumer({ groupId: "user-1" });
   await consumer.connect();
+
+  await consumer.subscribe({ topics: ["rider-updates"] });
 }
