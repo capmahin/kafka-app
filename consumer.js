9ea02f7bc76c0fabb4757517f5af974f1f1f1e52
capmahin <mahinulhuque1996@gmail.com>
2024-10-11 01:58:51 +0600

    addd

--- consumer.js
+++ consumer.js
@@ -4,13 +4,13 @@ async function init() {
   const consumer = kafka.consumer({ groupId: "user-1" });
   await consumer.connect();
 
-  await consumer.subscribe({ topics: ["rider-updates"] });
+  await consumer.subscribe({ topics: ["rider-updates"], fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
       console.log(`[${topic}: PART:${partition}: ${message.toString()}]`);
     }
   });
-
-  await consumer.disconnect();
 }
+
+init();
