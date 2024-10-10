1aaae9edc78cec92b9c816927811e0127058c58e
capmahin <mahinulhuque1996@gmail.com>
2024-10-11 02:13:02 +0600

    add lr

--- consumer.js
+++ consumer.js
@@ -8,7 +8,7 @@ async function init() {
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
-      console.log(`[${topic}: PART:${partition}}:`, message.toString());
+      console.log(`[${topic}: PART:${partition}}:`, message.value.toString());
     }
   });
 }
--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 async function init() {
   const producer = kafka.producer();
@@ -6,6 +12,13 @@ async function init() {
   console.log("Connecting Producer");
   await producer.connect();
   console.log("Producer Connected Successfully");
+
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+  });
 
   await producer.send({
     topic: "rider-updates",
